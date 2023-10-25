0692e02e029832c951cf661d5997578b2a88a765
yassinejoundi <yassinejoundi.abj@gmail.com>
2023-10-25 19:32:01 +0100

    update the head in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,20 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["/assets/css/main.css"],
+  app: {
+    head: {
+      title: "Reaction Time Test",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Test and improve your reaction time with the Reaction Time Test web application.",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/flash.png" }],
+    },
+  },
 })
